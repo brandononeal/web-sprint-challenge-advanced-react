a164b59543b22b7df4569a7e4b76b9de094bcd13
brandononeal <baoneal95@gmail.com>
2020-10-02 10:55:41 -0700

    mvp in progress

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,24 @@
-// write your custom hook here to control your checkout form
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useForm = (key, initialValues, cb) => {
+  const [values, setValues] = useLocalStorage(key, initialValues);
+
+  const handleChanges = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    if (e) e.preventDefault();
+    cb();
+  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setShowSuccessMessage(true);
+  // };
+
+  return [values, handleChanges, handleSubmit];
+};
